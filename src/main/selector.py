7155784afc4b092f5c0724c402a1887324d58a0e
Em Grasmeder <emma@gras.org>
2020-04-02 22:46:56 +0200

    em/pao #2 add main function. closes #2

--- src/main/selector.py
+++ src/main/selector.py
@@ -1,3 +1,4 @@
+import sys
 import numpy as np
 
 def choose(movies):
@@ -6,3 +7,10 @@ def choose(movies):
 def read_csv(file_path):
     with open(file_path, "r") as f:
         return list(map(lambda x: x.strip("\n"), f.readlines()))
+
+def main():
+    print(choose(read_csv(sys.argv[1])))
+
+    
+if __name__ == "__main__":
+    main()
